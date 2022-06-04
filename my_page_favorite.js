a6b762064a2b23b399c312413ca305cd89e7f411
dana0221 <dana8478@naver.com>
2022-06-04 17:38:34 +0900

    파일 링크 연결 및 마우스 오버 정리

---
+++ my_page_favorite.js
@@ -0,0 +1,9 @@
+// $(function(){
+//     $('#category').on('click', function(){
+//         $('#category>li ul').stop().slideDown();
+//     })
+//
+//     $('#sub_menu').on('mouseout',function(){
+//         $('#category>li ul').stop().slideUp();
+//     })
+// })
