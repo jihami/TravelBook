80c89d50a14a613ba1be965e1caa94834afa2863
jihami <w2027@e-mirim.hs.kr>
2022-06-15 01:15:53 +0900

    닉네임 받아오기

--- public/js/main_page.js
+++ public/js/main_page.js
@@ -1,18 +1,54 @@
 // 구글 인증 기능 추가
 var provider = new firebase.auth.GoogleAuthProvider();
-
+// db.collection('w2027@e-mirim.hs.kr').get().then(doc => {
+//     // if (doc.size <= 1){
+//     //     location.href = "login_info.html";
+//     // }
+//     // console.log(doc.data())
+//     doc.forEach((item)=>{
+//         console.log(item.data());
+//         nick = item.data().nickName;
+//         console.log(nick)
+//
+//         // if (nick == '여행하지'){
+//         //     console.log("dd")
+//         //     doc.push(doc);
+//         //     return false;
+//         // }
+//     })
+// });
+// var docRef = db.collection('w2027@e-mirim.hs.kr').doc("3405_김지함");
+// docRef.get().then(function(querySnapshot) {
+//     if (querySnapshot.exists) {
+//         for(let doc in querySnapshot.data()){
+//             // doc.dq
+//             // console.log(querySnapshot.data()[nickName])
+//             console.log( `key : ${doc}, value : ${querySnapshot.data()[doc]}` )
+//
+//
+//         }
+//
+//     } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//     }
+// }).catch(function(error) {
+//     console.log("Error getting document:", error);
+// });
+db.collection("w2027@e-mirim.hs.kr").doc("3405_김지함")
+    .onSnapshot(function(doc) {
+        console.log("Current data: ", doc.data());
+        console.log("nickname: ", doc.data().nickName);
+    });
 function login(){
     // 인증하기
     firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // var token = result.credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
 
-        console.log(user)		// 인증 후 어떤 데이터를 받아오는지 확인해보기 위함.
-        console.log(user.displayName)
-        console.log(user.email)
-        document.getElementById("login").innerHTML = user.displayName
+        // console.log(user)		// 인증 후 어떤 데이터를 받아오는지 확인해보기 위함.
+        // console.log(user.displayName)
+        // console.log(user.email)
+        document.getElementById("login").innerHTML = user.displayName;
 
         // 로그인 완료 시 로그아웃 버튼 보이기
         $(function(){
@@ -20,33 +56,30 @@ function login(){
         })
 
         // 세션에 email 사용하여 저장
-        sessionStorage.setItem("email", user.email)
-        sessionStorage.setItem("name", user.displayName)
-        const email = user.email
-        const name = user.displayName
-        console.log(email, name)
+        sessionStorage.setItem("email", user.email);
+        sessionStorage.setItem("name", user.displayName);
+        const email = user.email;
+        const name = user.displayName;
+        // console.log(email, name);
         success()
         //닉네임 체크
         function success() { //저장완료시 결과페이지로 넘어가도록
             // db.collection(email).doc(email).add({"name":name,"email":email, info : 0}); // 내용을 하나더 만듦
             db.collection(email).doc(email).set({"name":name,"email":email}); // 내용을 덮어씀
-            db.collection('w2027@e-mirim.hs.kr').get().then(snap => {
-                if (snap.size <= 1){
-                    location.href = "login_info.html";
-                }
-                console.log(snap.size)
+            db.collection('w2027@e-mirim.hs.kr').get().then(doc => {
+                // if (doc.size <= 1){
+                //     location.href = "login_info.html";
+                // }
+                doc.forEach(item=>{
+                    console.log(item.data());
+                    console.log(item.nickName);
+                    // console.log(item.data().name.nickName);
+                })
+                console.log(doc.size);
+                console.log(doc);
             });
         }
-    })//.catch(function(error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-    //     // ...
-    // });
+    })
 }
 
 function logout() {
@@ -60,12 +93,9 @@ function logout() {
         })
 
         // 로그아웃 시 세션 삭제
-        sessionStorage.removeItem("email")
-        sessionStorage.removeItem("name")
-    })//.catch((error) => {
-    //     // An error happened.
-    //     console.log("logoutError")
-    // });
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("name");
+    })
 
 }
 
