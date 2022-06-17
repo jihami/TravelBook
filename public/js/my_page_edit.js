ae26d6a2f6be87a52b24d98336f9dc85367080e5
jihami <w2027@e-mirim.hs.kr>
2022-06-17 10:39:51 +0900

    프로필 수정

--- public/js/my_page_edit.js
+++ public/js/my_page_edit.js
@@ -66,7 +66,6 @@ function logout() {
     })
 
 }
-
 // 세션이 있다면 로그인 상태 유지
 $(function (){
     len = sessionStorage.length
@@ -79,12 +78,14 @@ $(function (){
         }else{
             document.getElementById('login').innerHTML = nickName
             document.getElementById('nickname').value = nickName;
-            document.getElementById('email').value = email;
             document.getElementById('info').value = info;
             $(function(){
                 $('#logout').show();
             })
         }
+    }else {
+        alert("로그인 하세요.")
+        location.href = "index.html";
     }
 })
 
@@ -107,3 +108,23 @@ $(function(){
         });
     },3000);
 });
+
+async function save(){
+    name = sessionStorage.getItem("name")
+    email = sessionStorage.getItem("email")
+    console.log("s")
+    const nickName = document.getElementById('nickname').value;
+    const info = document.getElementById('info').value;
+    if (info === "" || nickName === "") {
+        console.log("s")
+        alert("내용을 입력하세요")
+    } else {
+        // await db.collection(email).doc(email).add({"name":name,"email":email}); // 문서를 하나 더 만듦
+        await db.collection(email).doc(name).set({"nickName": nickName, "info": info}); // 내용을 덮어씀
+        // db.collection(email).doc("name").set({"name":name,"email":email, "nickname":nickName, "info":info}); // 내용을 덮어씀
+        alert("저장 완료!");
+        sessionStorage.setItem("nickName", nickName);
+        sessionStorage.setItem("info", info);
+        location.href = "index.html";
+    }
+}
