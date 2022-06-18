fa92908978f910bcf399fc7b506a8f5ee68e41bb
jihami <w2027@e-mirim.hs.kr>
2022-06-18 18:45:55 +0900

    select box

---
+++ public/js/my_page_write.js
@@ -0,0 +1,32 @@
+function categoryChange(continent) {
+    var europe = ["그리스", "덴마크", "스페인", "아이슬란드", "영국", "이탈리아", "프랑스", "헝가리"];
+    var north_america = ["멕시코", "미국", "캐나다", "쿠바"];
+    var south_america = ["베네수엘라", "볼리비아", "브라질", "아르헨티나", "페루"];
+    var asia = ["네팔", "대만", "대한민국", "라오스", "싱가포르", "일본","태국"];
+    var oceania = ["뉴질랜드", "호주"];
+    var africa = ["마다가스카르", "모로코", "케냐"];
+    var target = document.getElementById("country");
+    var con = continent.value
+    if(con === "europe") {
+       var d = europe;
+    } else if(con === "north_america") {
+       var d = north_america;
+    } else if(con === "south_america") {
+       var d = south_america;
+    }else if(con === "asia") {
+       var d = asia;
+    }else if(con === "oceania") {
+       var d = oceania;
+    }else{
+       var d = africa;
+    }
+    target.options.length = 0;
+    for (x in d) {
+        var opt = document.createElement("option");
+        opt.value = d[x];
+        opt.innerHTML = d[x];
+        target.appendChild(opt);
+
+        console.log($("#continent opt:selected").val());
+    }
+}
