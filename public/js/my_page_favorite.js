fd045777fb0bfc9b9ef8f584cb519715ed2d0e55
jihami <w2027@e-mirim.hs.kr>
2022-06-21 08:22:52 +0900

    찜행지

--- public/js/my_page_favorite.js
+++ public/js/my_page_favorite.js
@@ -1,3 +1,10 @@
+// 세션이 있다면 로그인 상태 유지
+$(function (){
+    if(len === 0){
+        alert("로그인하세요.")
+        location.href = "index.html"
+    }
+})
 $(function(){
     $('#category').on('click', function(){
         $('#category>li ul').stop().slideDown();
