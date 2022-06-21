965b9c6299693f637bad60e9b5d5b55ac36a4c0c
jihami <w2027@e-mirim.hs.kr>
2022-06-21 11:06:09 +0900

    데이터 베이스 저장

--- public/js/travel.js
+++ public/js/travel.js
@@ -1,6 +1,27 @@
 nickName = sessionStorage.getItem("nickName")
 email = sessionStorage.getItem("email")
-
+// 세션이 있다면 로그인 상태 유지
+$(function (){
+    len = sessionStorage.length
+    photoURL = sessionStorage.getItem("photoURL")
+    nickName = sessionStorage.getItem("nickName")
+    email = sessionStorage.getItem("email")
+    info = sessionStorage.getItem("info")
+    if(len !== 0){ //로그인 되어 있으면
+        if(nickName == null){
+            location.href = "login_info.html";
+        }else{
+            document.getElementById("userIcon").src = photoURL;
+            document.getElementById('login').innerHTML = nickName;
+            $(function(){
+                $('#logout').show();
+            })
+        }
+    }else {
+        alert("로그인하세요.")
+        location.href = "index.html"
+    }
+})
 db.collection(nickName+"찜행지").get().then((data) => {
     // console.log(data.size)
     // console.log(data)
