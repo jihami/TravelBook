c61cd4b16afa40002f098c524fe456ef5e73a628
jihami <w2027@e-mirim.hs.kr>
2022-06-21 16:16:32 +0900

    팁 기록, 공개기록 보기

--- public/js/travel_page_public.js
+++ public/js/travel_page_public.js
@@ -1,5 +1,6 @@
 nickName = sessionStorage.getItem("nickName")
 email = sessionStorage.getItem("email")
+info = sessionStorage.getItem("info")
 // 세션이 있다면 로그인 상태 유지
 $(function (){
     len = sessionStorage.length
@@ -22,25 +23,26 @@ $(function (){
     }
 })
 
-function set(country){
+function setCon(country){
     db.collection(country).get().then((data) => {
-        if (data.size < 1) {
-            // location.href = "login_info.html";
-            document.getElementById('record_list').innerHTML = "기록이 없습니다."
-        }else {
+        if (data.size >= 1) {
+            document.getElementById("record_list").innerHTML = "";
             data.forEach((doc) => {
                 docId = doc.id
                 con = country
-                console.log(docId)
-                console.log(con)
-                console.log(doc.data())
+                // console.log(docId)
+                // console.log(con)
+                // console.log(doc.data())
                 let themp = `<div class="record">
                     <img src="../img/diary.png" onclick="recordP(docId, con)">
                     <p>${doc.data().title} / ${doc.data().name}</p>
                     <hr style="height:1px; background-color:black;">
                     <p>${doc.data().startDay} ~ ${doc.data().endDay}</p>
                 </div>`
+                console.log(1)
                 $("#record_list").append(themp)
+
+                console.log(2)
             })
         }
     })
@@ -51,27 +53,40 @@ function recordP(id,country){
 
     sessionStorage.removeItem("country");
     sessionStorage.setItem("country", country);
+    sessionStorage.setItem("DocId", id);
 
     location.href = "record_page_public.html"
-    console.log(id,country)
-    return id,country
+    // console.log(id,country)
+    return id, country
 }
-id = sessionStorage.getItem("DocId")
-con = sessionStorage.getItem("country")
-console.log(id)
-db.collection(con).doc(id).get().then(function(da) {
-    if (da.exists) {
-        dt = da.data()
-        console.log(dt)
-        document.getElementById('con').innerHTML = dt.title+" - "+dt.name;
-        document.getElementById('day').innerHTML = dt.startDay + " ~ " + dt.endDay;
-        document.getElementById("text1").innerHTML = dt.text1
-        document.getElementById("text2").innerHTML = dt.text2
-        document.getElementById("text3").innerHTML = dt.text3
-        document.getElementById("text4").innerHTML = dt.text4
-        document.getElementById("img1").src = dt.Img1
-        document.getElementById("img2").src = dt.Img2
-        document.getElementById("img3").src = dt.Img3
-        document.getElementById("img4").src = dt.Img4
-    }
-})
+
+function submitTip(con){
+    let tip = document.getElementById("tip_write").value;
+    let photoURL = sessionStorage.getItem("photoURL")
+    let info = sessionStorage.getItem("info")
+    let name = sessionStorage.getItem("nickName")
+    db.collection(con+"TIP").add({"name":name,"info":info,"photoURL":photoURL, 'tip' : tip});
+    document.getElementById("tip_write").value="";
+    alert("저장 완료!");
+    setTip(con);
+}
+function setTip(country) {
+    db.collection(country + "TIP").get().then((data) => {
+        if (data.size >= 1) {
+            document.getElementById("tip_list").innerHTML = "";
+            data.forEach((doc) => {
+                // console.log(doc.data().name)
+                // console.log(doc.data().photoURL)
+                // console.log(doc.data().tip)
+                console.log(doc.data())
+                let themp = `<div class="tip">
+                            <p class="user_id">${doc.data().name} - ${doc.data().info}</p>
+                            <img src="${doc.data().photoURL}">
+                            <p class="tip_subject">${doc.data().tip}</p>
+                        </div>`
+                $("#tip_list").append(themp)
+            })
+        }
+    })
+
+}
