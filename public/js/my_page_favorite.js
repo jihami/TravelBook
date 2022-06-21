531b277eba1191f58bea9a1ea9c6db57744b3860
jihami <w2027@e-mirim.hs.kr>
2022-06-21 09:46:24 +0900

    찜행지 보이게 끗!

--- public/js/my_page_favorite.js
+++ public/js/my_page_favorite.js
@@ -14,3 +14,25 @@ $(function(){
         $('#category>li ul').stop().slideUp();
     })
 })
+nickName = sessionStorage.getItem("nickName")
+
+db.collection(nickName+"찜행지").get().then((data) => {
+    // console.log(data.size)
+    // console.log(data)
+    if (data.size < 1) {
+        // location.href = "login_info.html";
+        document.getElementById('list').innerHTML = "기록이 없습니다."
+    }else {
+        data.forEach((doc) => {
+            country = doc.data().country
+            countryk = doc.data().countryk
+            console.log(country)
+            console.log(doc.data())
+            let themp = `<a href="travel_${country}.html"><div class="place">
+                    <img src="../img/${country}/flag.png" alt="img" style="width:172px; height:110px;">
+                    <p class="place_title">${countryk}</p>
+                </div></a>`
+            $("#list").append(themp)
+        })
+    }
+})
