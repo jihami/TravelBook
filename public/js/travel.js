ded8e877c98260694478fe036198d3ea0fedfe04
jihami <w2027@e-mirim.hs.kr>
2022-06-21 09:24:53 +0900

    찜행지 추가

--- public/js/travel.js
+++ public/js/travel.js
@@ -1,23 +1,32 @@
-let click_chk = 0
-
-function favorite_on(country){
-    if(click_chk === 0){
-        $(function(){
-            $('#' + country).css({
-                filter:'opacity(100%)'
-            })
+nickName = sessionStorage.getItem("nickName")
+email = sessionStorage.getItem("email")
+db.collection(nickName+"찜행지").get().then((data) => {
+    // console.log(data.size)
+    // console.log(data)
+    if (data.size < 1) {
+        console.log("기록 없음")
+    }else {
+        data.forEach((doc) => {
+            // console.log(id)
+            console.log(doc.data().country)
+            country = doc.data().country
+            $(`#${country}`).hide()
         })
-
-        click_chk = 1
-    } else{
-        $(function(){
-            $('#' + country).css({
-                filter:'opacity(50%)'
-            })
-        })
-
-        click_chk = 0
     }
+})
+function favorite_on(country){
+    $(function (){
+        if(len === 0){
+            alert("로그인하세요.")
+        }else {
+            if(confirm(country+"를 찜행지에 추가하시겠습니까?")===true){
+                db.collection(nickName+"찜행지").add({ country:country});
+                alert("추가 완료")
+                $(`#${country}`).hide()
+            }
+        }
+    })
+
 }
 
 $(function(){
