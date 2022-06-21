995c5f262f8c500042eac97cf67ea574b30d34ee
jihami <w2027@e-mirim.hs.kr>
2022-06-21 09:42:03 +0900

    찜행지 추가 db구조 재설정

--- public/js/travel.js
+++ public/js/travel.js
@@ -1,11 +1,10 @@
 nickName = sessionStorage.getItem("nickName")
 email = sessionStorage.getItem("email")
+
 db.collection(nickName+"찜행지").get().then((data) => {
     // console.log(data.size)
     // console.log(data)
-    if (data.size < 1) {
-        console.log("기록 없음")
-    }else {
+    if (data.size>=1) {
         data.forEach((doc) => {
             // console.log(id)
             console.log(doc.data().country)
@@ -14,13 +13,13 @@ db.collection(nickName+"찜행지").get().then((data) => {
         })
     }
 })
-function favorite_on(country){
+function favorite_on(country, name){
     $(function (){
         if(len === 0){
             alert("로그인하세요.")
         }else {
             if(confirm(country+"를 찜행지에 추가하시겠습니까?")===true){
-                db.collection(nickName+"찜행지").add({ country:country});
+                db.collection(nickName+"찜행지").add({ country:country, countryk:name});
                 alert("추가 완료")
                 $(`#${country}`).hide()
             }
